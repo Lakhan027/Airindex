9664d790f1912754b5612ccf80f67515bb01d1e3
Lakhan Sharma <91717865+Lakhan027@users.noreply.github.com>
2023-07-26 20:08:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from flask_ngrok import run_with_ngrok
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('/content/drive/My Drive/Aqi2.pkl','rb'))
+run_with_ngrok(app)
+
+@app.route('/')
+def home():
+  return render_template("index2.html")
+
+@app.route('/predict',methods=['GET'])
+def predict():
+
+
+    '''
+    For rendering results on HTML GUI
+    '''
+    city = float(request.args.get('city'))
+    pm25 = float(request.args.get('pm25'))
+    pm10 = float(request.args.get('pm10'))
+    no = float(request.args.get('no'))
+    no2 = float(request.args.get('no2'))
+    nox = float(request.args.get('nox'))
+    nh3 = float(request.args.get('nh3'))
+    co = float(request.args.get('co'))
+    so3 = float(request.args.get('so3'))
+    o3 = float(request.args.get('o3'))
+    benzene = float(request.args.get('benzene'))
+    toluene = float(request.args.get('toluene'))
+    xylene = float(request.args.get('xylene'))
+
+
+
+
+
+    prediction = model.predict([[city, pm25, pm10, no, no2, nox, nh3, co, so3, o3, benzene, toluene, xylene]])
+
+
+    return render_template('index2.html', prediction_text='Predicted Air Quality Index : {}'.format(prediction))
+
+
+app.run()
